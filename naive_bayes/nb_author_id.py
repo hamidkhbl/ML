63ae4e90c95ac9b5c6248e09cb9750b1a1be4178
Hamidkhbl <hamidkhbl@gmail.com>
2020-03-16 17:05:16 -0600

    Refactore code

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
@@ -11,91 +9,18 @@
 """
     
 import sys
+import os
 from time import time
 import pickle
 import numpy
+path = os.path.join(os.path.dirname(__file__), '../tools/')
+sys.path.insert(1,path)
+from email_preprocess import preprocess
+from file_op import dos2unix
 
-from sklearn.model_selection import cross_validate
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_selection import SelectPercentile, f_classif
-
-def fixFile(original,destination):
-    content = ''
-    outsize = 0
-    with open(original, 'rb') as infile:
-        content = infile.read()
-
-    with open(destination, 'wb') as output:
-        for line in content.splitlines():
-            outsize += len(line) + 1
-            output.write(line + str.encode('\n'))
-
-    print("Done. Saved %s bytes." % (len(content)-outsize))
-
-fixFile("F:/Google Drive/git/ML/naive_bayes/email_authors.pkl","F:/Google Drive/git/ML/naive_bayes/email_authors_fixed.pkl")
-fixFile("F:/Google Drive/git/ML/naive_bayes/word_data.pkl","F:/Google Drive/git/ML/naive_bayes/word_data_fixed.pkl")
-
-def preprocess():
-    words_file = "F:/Google Drive/git/ML/naive_bayes/word_data_fixed.pkl"
-    authors_file="F:/Google Drive/git/ML/naive_bayes/email_authors_fixed.pkl"
-    """ 
-        this function takes a pre-made list of email texts (by default word_data.pkl)
-        and the corresponding authors (by default email_authors.pkl) and performs
-        a number of preprocessing steps:
-            -- splits into training/testing sets (10% testing)
-            -- vectorizes into tfidf matrix
-            -- selects/keeps most helpful features
-
-        after this, the feaures and labels are put into numpy arrays, which play nice with sklearn functions
-
-        4 objects are returned:
-            -- training/testing features
-            -- training/testing labels
-
-    """
-
-
-
-    ### the words (features) and authors (labels), already largely preprocessed
-    ### this preprocessing will be repeated in the text learning mini-project
-
-
-    authors_file_handler = open(authors_file, "rb")
-    authors = pickle.load(authors_file_handler)
-    authors_file_handler.close()
-
-    words_file_handler = open(words_file, "rb")
-    word_data = pickle.load(words_file_handler)
-    words_file_handler.close()
-
-    ### test_size is the percentage of events assigned to the test set
-    ### (remainder go into training)
-    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
-
-
-
-    ### text vectorization--go from strings to lists of numbers
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english')
-    features_train_transformed = vectorizer.fit_transform(features_train)
-    features_test_transformed  = vectorizer.transform(features_test)
-
-
-
-    ### feature selection, because text is super high dimensional and 
-    ### can be really computationally chewy as a result
-    selector = SelectPercentile(f_classif, percentile=10)
-    selector.fit(features_train_transformed, labels_train)
-    features_train_transformed = selector.transform(features_train_transformed).toarray()
-    features_test_transformed  = selector.transform(features_test_transformed).toarray()
-
-    ### info on the data
-    print("no. of Chris training emails:", sum(labels_train))
-    print("no. of Sara training emails:", len(labels_train)-sum(labels_train))
-    
-    return features_train_transformed, features_test_transformed, labels_train, labels_test
-
+# Fix files, only run it once if the files are not in a correct formst
+dos2unix("F:/Google Drive/git/ML/naive_bayes/email_authors.pkl","F:/Google Drive/git/ML/naive_bayes/email_authors_fixed.pkl")
+dos2unix("F:/Google Drive/git/ML/naive_bayes/word_data.pkl","F:/Google Drive/git/ML/naive_bayes/word_data_fixed.pkl")
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
