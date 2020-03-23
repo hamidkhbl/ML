d57a33b63f81dbe44e8d1862a54b341f6edb3ac9
Hamidkhbl <hamidkhbl@gmail.com>
2020-03-23 08:44:48 -0600

    Train decision tree

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -1,1 +1,23 @@
+#%%
+# Imports
 from sklearn import tree
+from sklearn import metrics
+import sys
+import os
+path = os.path.join(os.path.dirname(__file__), '../tools/')
+sys.path.insert(1,path)
+from email_preprocess import preprocess
+from file_op import dos2unix
+
+# %% 
+dos2unix("../tools/email_authors.pkl","../tools/email_authors_fixed.pkl")
+dos2unix("../tools/word_data.pkl","../tools/word_data_fixed.pkl")
+
+features_train, features_test, labels_train, labels_test = preprocess()
+# %%
+# Train and predict
+clf = tree.DecisionTreeClassifier()
+pred = clf.fit(features_train, labels_train).predict(features_test)
+print("Accuracy:",metrics.accuracy_score(labels_test, pred))
+
+# %%
--- NaiveBayes/nb_author_id.py
+++ NaiveBayes/nb_author_id.py
@@ -7,7 +7,8 @@
     Sara has label 0
     Chris has label 1
 """
-    
+#%%
+# imports 
 import sys
 import os
 from time import time
@@ -18,24 +19,22 @@ sys.path.insert(1,path)
 from email_preprocess import preprocess
 from file_op import dos2unix
 
+#%%
 # Fix files, only run it once if the files are not in a correct formst
-dos2unix("F:/Google Drive/git/ML/naive_bayes/email_authors.pkl","F:/Google Drive/git/ML/naive_bayes/email_authors_fixed.pkl")
-dos2unix("F:/Google Drive/git/ML/naive_bayes/word_data.pkl","F:/Google Drive/git/ML/naive_bayes/word_data_fixed.pkl")
+dos2unix("../tools/email_authors.pkl","../tools/email_authors_fixed.pkl")
+dos2unix("../tools/word_data.pkl","../tools/word_data_fixed.pkl")
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#%%
 
-
-
-#########################################################
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 clf = GaussianNB()
 pred = clf.fit(features_train, labels_train).predict(features_test)
 print("Accuracy:",metrics.accuracy_score(labels_test, pred))
 #########################################################
-
-
+# %%
